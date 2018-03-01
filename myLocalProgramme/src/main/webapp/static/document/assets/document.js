99f279928484dc73cc69f510d46127b0c866d219
RosalynWu <yulin990422@126.com>
2018-03-01 09:30:41 +0800

    新悬浮框

--- myLocalProgramme/src/main/webapp/static/document/assets/document.js
+++ myLocalProgramme/src/main/webapp/static/document/assets/document.js
@@ -12,17 +12,9 @@ angular.module('docApp',['ui.router'])
             .state('popup_angular', {
                 url: "/popup_angular",
                 templateUrl: 'templates/popup_angular.html'
-            })
-            .state('bubble', {
-                url: "/bubble",
-                templateUrl: 'templates/bubble.html'
             });
     })
     .controller('myCtrl',['$scope',function($scope){
-        $scope.clickTest = function(){
-            console.log("被点击了");
-        };
-        $scope.array = [1,2,3];
         $scope.config = {
             showStyle: 'click',
             title: '',
@@ -87,7 +79,7 @@ angular.module('docApp',['ui.router'])
             scope:{defaultConfig : '='},
             link : function(scope, elem, attr){
                 //初始化配置信息
-                var defaultConfig = {
+              var defaultConfig = {
                     showStyle: 'click',
                     title: '默认提示信息',
                     replaceTpl:''
@@ -167,167 +159,4 @@ angular.module('docApp',['ui.router'])
                 }
             }
         }
-    })
-    .directive('bubble',function($compile){
-        return{
-            template: '<div class="ui-popup ui-popup-anchor">' +
-            '<div class="ui-bubble">' +
-            '<div ng-transclude class="ui-bubble-content"></div>' +
-            '</div>' +
-            '</div>',
-            restrict:'AE',
-            transclude: true,
-            replace: true,
-            scope:{
-                'ngIf': '=',
-                'ngShow': '=',
-                'ngHide': '=',
-                //吸附到指定 ID 元素
-                'for': '@',
-                'close': '&',
-                //关闭悬浮框动作
-                'closeAction': '@',
-                // 显示持续时间
-                'duration': '@'
-            },
-            controller:['$scope',function($scope){
-                // 默认的触发关闭动作
-                this.closeAction = ['esc', 'timeout'];
-
-                this.close = function(isEvent){
-                    $scope.close();
-                    if(isEvent){
-                        $scope.$apply();
-                    }
-                }
-
-            }],
-            link: function(scope, elem, attrs, controller) {
-                var $ = angular.element;
-                var noop = function() {};
-                var popup = {
-                    node:elem[0],
-                    showElement: noop,
-                    hideElement: noop,
-                    removeElement: noop
-                };
-                var temp = fix(elem);
-                var $document = $(document);
-
-                if (attrs.closeAction) {
-                    controller.closeAction = attrs.closeAction.split(/\s+/);
-                }
-
-                // 模型同步UI显示、隐藏事件
-                if (attrs.ngIf) scope.$watch('ngIf', change);
-                if (attrs.ngShow) scope.$watch('ngShow', change);
-
-                elem.one('$destroy', function() {
-                    change(false);
-                    popup.remove();
-                    temp.remove();
-                });
-
-                // UI的显示与隐藏、删除事件
-                function change(open){
-                    var anchor = getAnchor(attrs['for']);
-                    if (open) {
-                        // 使用 setTimeout 等待 ng-show 在 UI 上生效
-                        elem.css('visibility', 'hidden');
-                        setTimeout(function() {
-                            elem.css('visibility', 'visible');
-                            setEvent(open);
-                        }, 0);
-                    } else {
-                        popup.close();
-                        setEvent(open);
-                    }
-                }
-
-                function getAnchor(id) {
-                    return document.getElementById(id);
-                }
-
-                // ESC 快捷键关闭悬浮框
-                function esc(event){
-                    var target = event.target;
-                    var nodeName = target.nodeName;
-                    var rinput = /^input|textarea$/i;
-                    var isBlur = elem[0];
-                    var isInput = rinput.test(nodeName) && target.type !== 'button';
-                    var keyCode = event.keyCode;
-
-                    // 避免输入状态中 ESC 误操作关闭
-                    if (!isBlur || isInput) {
-                        return;
-                    }
-                    if (keyCode === 27) {
-                        controller.close(event);
-                    }
-                }
-
-                // 外部点击关闭
-                function outerclick(event) {
-                    if (!elem[0].contains(event.target)) {
-                        controller.close(event);
-                    }
-                }
-
-                // 定时关闭
-                function timeout() {
-                    if (attrs.duration) {
-                        timeout.timer = setTimeout(function() {
-                            controller.close(true);
-                        }, Number(attrs.duration));
-                    }
-                }
-
-                function setEvent(open) {
-                    controller.closeAction.forEach(function(action) {
-                        switch (action) {
-                            case 'esc':
-                                if (open) {
-                                    $document.on('keydown', esc);
-                                } else {
-                                    $document.off('keydown', esc);
-                                }
-                                break;
-                            case 'timeout':
-                                if (open) {
-                                    timeout();
-                                } else {
-                                    clearTimeout(timeout.timer);
-                                }
-                                break;
-                            case 'outerchick':
-                                if (open) {
-                                    $document
-                                        .on('ontouchend', outerclick)
-                                        .on('click', outerclick);
-                                } else {
-                                    $document
-                                        .off('ontouchend', outerclick)
-                                        .off('click', outerclick);
-                                }
-                                break;
-                            case 'click':
-                                //case 'focusout': // Error: [$rootScope:inprog]
-                                if (open) {
-                                    elem.on(action, controller.close);
-                                } else {
-                                    elem.off(action, controller.close);
-                                }
-                                break;
-                        }
-                    });
-                }
-
-            }
-        }
     });
-function fix(elem) {
-    var temp = document.createElement('popup');
-    document.body.appendChild(temp);
-    temp.appendChild(elem[0]);
-    return $(temp);
-}
